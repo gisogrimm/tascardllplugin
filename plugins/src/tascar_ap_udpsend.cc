3854e9b828e63376fcdb09d792708825e09783f8
Giso Grimm <giso.grimm@vegri.net>
2021-05-12 13:19:30 +0200

    first implementation of UDP audio sender

--- plugins/src/tascar_ap_udpsend.cc
+++ plugins/src/tascar_ap_udpsend.cc
@@ -1,5 +1,6 @@
 #include "netaudio.h"
 #include <tascar/audioplugin.h>
+#include <udpsocket.h>
 
 /*
   This example implements an audio plugin which is a white noise
@@ -8,45 +9,82 @@
   Audio plugins inherit from TASCAR::audioplugin_base_t and need to
   implement the method ap_process(), and optionally add_variables().
  */
-class noise_t : public TASCAR::audioplugin_base_t {
+class udpsend_t : public TASCAR::audioplugin_base_t {
 public:
-  noise_t(const TASCAR::audioplugin_cfg_t& cfg);
+  udpsend_t(const TASCAR::audioplugin_cfg_t& cfg);
   void ap_process(std::vector<TASCAR::wave_t>& chunk, const TASCAR::pos_t& pos,
                   const TASCAR::zyx_euler_t& o, const TASCAR::transport_t& tp);
-  void add_variables(TASCAR::osc_server_t* srv);
-  virtual ~noise_t();
+  virtual ~udpsend_t();
+  void configure();
+  void release();
 
 private:
-  double a;
+  udpsocket_t socket;
+  std::string host;
+  int32_t port;
+  netaudio_info_t info;
+  char* cbuffer;
+  size_t cbufferlen;
+  size_t cyclecounter;
+  netaudio_err_t errcode;
+  float* audiobuffer;
+  size_t sample_index;
 };
 
 // default constructor, called while loading the plugin
-noise_t::noise_t(const TASCAR::audioplugin_cfg_t& cfg)
-    : audioplugin_base_t(cfg), a(0.001)
+udpsend_t::udpsend_t(const TASCAR::audioplugin_cfg_t& cfg)
+    : audioplugin_base_t(cfg), host("localhost"), port(0), cbuffer(NULL),
+      cbufferlen(0), cyclecounter(0), audiobuffer(NULL), sample_index(0)
 {
   // register variable for XML access:
-  GET_ATTRIBUTE_DBSPL(a,"noise level");
+  GET_ATTRIBUTE(host, "", "destination host");
+  GET_ATTRIBUTE(port, "", "destination port number");
+  socket.bind(port);
+  socket.set_destination(host.c_str());
 }
 
-noise_t::~noise_t() {}
-
-void noise_t::add_variables(TASCAR::osc_server_t* srv)
+void udpsend_t::configure()
 {
-  // register variables for interactive OSC access:
-  srv->add_double_dbspl("/a", &a);
+  info = new_netaudio_info(f_sample, pcm16bit, n_channels, n_fragment);
+  cbufferlen = std::max(get_buffer_length_header(), get_buffer_length(info));
+  cbuffer = new char[cbufferlen];
+  cyclecounter = 0;
+  audiobuffer = new float[n_channels * n_fragment];
 }
 
-void noise_t::ap_process(std::vector<TASCAR::wave_t>& chunk,
-                         const TASCAR::pos_t& pos, const TASCAR::zyx_euler_t& o,
-                         const TASCAR::transport_t& tp)
+void udpsend_t::release()
+{
+  delete[] cbuffer;
+  delete[] audiobuffer;
+}
+
+udpsend_t::~udpsend_t() {}
+
+void udpsend_t::ap_process(std::vector<TASCAR::wave_t>& chunk,
+                           const TASCAR::pos_t& pos,
+                           const TASCAR::zyx_euler_t& o,
+                           const TASCAR::transport_t& tp)
 {
   // implement the algrithm:
-  for(uint32_t k = 0; k < chunk[0].n; ++k)
-    chunk[0].d[k] += 2.0 * a * (TASCAR::drand() - 0.5);
+  if(!cyclecounter) {
+    cyclecounter = std::max(1.0, f_fragment);
+    size_t codedbytes(encode_header(info, cbuffer, cbufferlen, errcode));
+    socket.send(cbuffer, codedbytes, port);
+    // ignore errors for now.
+  } else {
+    --cyclecounter;
+  }
+  for(size_t k = 0; k < n_fragment; ++k)
+    for(size_t c = 0; c < n_channels; ++c)
+      audiobuffer[c + n_channels * k] = chunk[c][k];
+  size_t codedbytes(encode_audio(info, audiobuffer, n_fragment * n_channels,
+                                 sample_index, cbuffer, cbufferlen, errcode));
+  sample_index += n_fragment;
+  socket.send(cbuffer, codedbytes, port);
 }
 
 // create the plugin interface:
-REGISTER_AUDIOPLUGIN(noise_t);
+REGISTER_AUDIOPLUGIN(udpsend_t);
 
 /*
  * Local Variables:
